4763621577b69c26059853699705f722e1cc59a2
davidgao <3594008212@qq.com>
2025-10-27 19:02:00 -0700

    removed deprecated database op

--- src/database/sqlite.rs
+++
@@ -1,8 +0,0 @@
-use rusqlite::Connection;
-
-use crate::database::package_data::PackageData;
-
-// pub fn get_all_package_data(conn: &Connection) -> Result<Vec<PackageData>, String> {
-//     let mut stmt = conn.prepare("SELECT * FROM").map_err(|e| e.to_string());
-
-// }
